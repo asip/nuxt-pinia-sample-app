fdec6fd31c29ff7e939226360aeb5f0832eade3f
Yasumasa Ashida <ys.ashida@gmail.com>
2026-05-29 18:30:36 +0900

    fix(store): Fix comments in use-counter-store

--- app/stores/use-counter-store.ts
+++ app/stores/use-counter-store.ts
@@ -12,12 +12,12 @@ export const useCounterStore = defineStore(
     return { count, increment, decrement };
   },
 
-  //persistを追加して状態を保持する
+  // Add persist option and save state (persistを追加して状態を保持する)
   {
     persist: {
-      // セッションストレージに保存する場合
+      // When saving to session storage (セッションストレージに保存する場合)
       storage: piniaPluginPersistedstate.sessionStorage()
-      // クッキーに保存する場合
+      // When saving to cookie (クッキーに保存する場合)
       // storage: piniaPluginPersistedstate.cookies,
     }
   }
